0f591b4caca73383c38a81aa9f8236f46ef9d2df
delianides <github@drew.delianides.com>
2021-10-29 12:58:30 -0400

    (add) environment variables to zsh

--- tag-zsh/zsh/configs/export.zsh
+++ tag-zsh/zsh/configs/export.zsh
@@ -1,3 +1,4 @@
 export NODEJS_CHECK_SIGNATURES=no
+export BREW_NO_AUTO_UPDATE=1
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
