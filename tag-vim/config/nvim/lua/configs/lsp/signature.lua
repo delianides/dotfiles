148a9f28aaf89dcd7a87a6bdc4af84138ab1c23d
delianides <github@drew.delianides.com>
2022-03-29 15:18:19 -0400

    update: added lsp_signature

---
+++ tag-vim/config/nvim/lua/configs/lsp/signature.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+function M.config()
+	local status_ok, lsp_signature = pcall(require, "lsp_signature")
+	if not status_ok then
+		return
+	end
+
+	lsp_signature.setup()
+end
+
+return M
