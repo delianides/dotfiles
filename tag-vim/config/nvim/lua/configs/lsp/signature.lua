9130b7cc68f5ae6939ebccd00ad43ff75c9d164a
Drew Delianides <drew@delianides.com>
2022-03-29 20:56:42 -0400

    fix: formatting

--- tag-vim/config/nvim/lua/configs/lsp/signature.lua
+++ tag-vim/config/nvim/lua/configs/lsp/signature.lua
@@ -1,12 +1,12 @@
 local M = {}
 
 function M.config()
-	local status_ok, lsp_signature = pcall(require, "lsp_signature")
-	if not status_ok then
-		return
-	end
+  local status_ok, lsp_signature = pcall(require, "lsp_signature")
+  if not status_ok then
+	return
+  end
 
-	lsp_signature.setup()
+  lsp_signature.setup()
 end
 
 return M
