fde1658e297e3e0a7fc5bd06546095174cb35b88
delianides <github@drew.delianides.com>
2023-10-06 11:59:02 -0400

    update: removed null-ls

--- tag-vim/config/nvim/lua/plugins/lsp.lua
+++ tag-vim/config/nvim/lua/plugins/lsp.lua
@@ -34,7 +34,7 @@ return {
     },
   },
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     version = false,
     opts = function(_, opts)
       local nls = require("null-ls")
