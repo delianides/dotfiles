934705b66831bdc39a312134ede41ff85a7fc880
delianides <github@drew.delianides.com>
2026-03-20 14:13:51 -0400

    updated with new smaller footprint

---
+++ tag-editor/config/nvim/lua/plugins/extras/lang/python.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(ev)
+          vim.keymap.del("n", "K", { buffer = ev.buf })
+        end,
+      })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        python = { "ruff" },
+      },
+    },
+  },
+}
