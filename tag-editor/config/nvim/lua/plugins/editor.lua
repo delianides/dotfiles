c9b04c0a60bf3fd1e8cb0a1fb652083e2a4ee87d
delianides <github@drew.delianides.com>
2026-04-21 10:40:35 +0200

    nvim: enable additional LazyVim extras and swap to fff.nvim picker
    Adds extras for nvim-cmp, snacks_explorer, snacks_picker, gh, mini-
    hipatterns, and rest. Removes unused plugins (git-conflict, tabout,
    nvim-highlight-colors) and adds fff.nvim as a fast file/live-grep
    picker with ff/fg/fz/fc keymaps.

--- tag-editor/config/nvim/lua/plugins/editor.lua
+++ tag-editor/config/nvim/lua/plugins/editor.lua
@@ -1,27 +1,11 @@
 return {
   {
-    "akinsho/git-conflict.nvim",
-    event = "BufReadPre",
-    config = true,
-  },
-  {
     "christoomey/vim-tmux-navigator",
     event = "BufReadPre",
   },
   {
-    "abecodes/tabout.nvim",
-    config = true,
-    enabled = false,
-  },
-  {
     "max397574/better-escape.nvim",
     config = true,
-  },
-  {
-    "brenoprata10/nvim-highlight-colors",
-    cmd = "HighlightColorsToggle",
-    opts = { enable_tailwind = true, enable_named_colors = true },
-    enable = false,
   },
   {
     "TimUntersberger/neogit",
@@ -36,5 +20,58 @@ return {
       disable_builtin_notifications = true,
     },
     dependencies = { "sindrets/diffview.nvim" },
+  },
+  {
+    "dmtrKovalenko/fff.nvim",
+    build = function()
+      -- this will download prebuild binary or try to use existing rustup toolchain to build from source
+      -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+      require("fff.download").download_or_build_binary()
+    end,
+    -- if you are using nixos
+    -- build = "nix run .#release",
+    opts = { -- (optional)
+      debug = {
+        enabled = true, -- we expect your collaboration at least during the beta
+        show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
+      },
+    },
+    -- No need to lazy-load with lazy.nvim.
+    -- This plugin initializes itself lazily.
+    lazy = false,
+    keys = {
+      {
+        "ff", -- try it if you didn't it is a banger keybinding for a picker
+        function()
+          require("fff").find_files()
+        end,
+        desc = "FFFind files",
+      },
+      {
+        "fg",
+        function()
+          require("fff").live_grep()
+        end,
+        desc = "LiFFFe grep",
+      },
+      {
+        "fz",
+        function()
+          require("fff").live_grep({
+            grep = {
+              modes = { "fuzzy", "plain" },
+            },
+          })
+        end,
+        desc = "Live fffuzy grep",
+      },
+      {
+        "fc",
+        function()
+          require("fff").live_grep({ query = vim.fn.expand("<cword>") })
+        end,
+        desc = "Search current word",
+      },
+    },
   },
 }
